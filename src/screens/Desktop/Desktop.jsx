40add9d26cc9ea27180017cddd9e5d3cd3a1ffbb
PrishaMaiti <prisha.maiti@gmail.com>
2026-02-01 14:21:14 -0800

    feat: add initial UI structure with HTML, CSS, and JavaScript components

---
+++ src/screens/Desktop/Desktop.jsx
@@ -0,0 +1,75 @@
+export const Desktop = () => {
+  return (
+    <div
+      className="overflow-hidden bg-[linear-gradient(180deg,rgba(255,255,255,1)_0%,rgba(255,255,255,0)_100%)] w-full min-w-[1280px] min-h-[1080px] relative"
+      data-model-id="1:6"
+    >
+      <div className="top-[698px] left-32 inline-flex flex-wrap items-center gap-[16px_16px] absolute">
+        <div className="inline-flex items-center justify-center gap-2 px-4 py-3 relative flex-[0_0_auto] bg-black rounded-xl">
+          <div className="relative flex items-center justify-center w-fit mt-[-1.00px] [font-family:'Inter',Helvetica] font-medium text-white text-lg text-center tracking-[-0.09px] leading-[26.1px] whitespace-nowrap">
+            Generate graph
+          </div>
+        </div>
+      </div>
+
+      <div className="top-[465px] left-[509px] w-[70px] h-[547px] absolute bg-[#d9d9d9]" />
+
+      <div className="top-[calc(50.00%_-_35px)] left-[54px] w-[313px] h-[70px] absolute bg-[#d9d9d9]" />
+
+      <div className="top-[calc(50.00%_-_35px)] left-[39px] w-[359px] h-[70px] absolute bg-[#d9d9d9]" />
+
+      <div className="top-[calc(50.00%_-_55px)] left-[39px] w-[359px] h-[70px] absolute bg-[#d9d9d9]" />
+
+      <div className="top-[calc(50.00%_-_145px)] left-[39px] w-[359px] h-[70px] absolute bg-[#d9d9d9]" />
+
+      <div className="top-[calc(50.00%_-_235px)] left-[39px] w-[359px] h-[70px] absolute bg-[#d9d9d9]" />
+
+      <div className="top-[calc(50.00%_-_325px)] left-[39px] w-[359px] h-[70px] absolute bg-[#d9d9d9]" />
+
+      <div className="absolute top-[518px] left-[177px] h-11 flex items-center justify-center [font-family:'Inter',Helvetica] font-medium text-black text-3xl text-center tracking-[-0.15px] leading-[43.5px] whitespace-nowrap">
+        Major
+      </div>
+      <div className="absolute top-[608px] left-[104px] h-11 flex items-center justify-center [font-family:'Inter',Helvetica] font-medium text-black text-3xl text-center tracking-[-0.15px] leading-[43.5px] whitespace-nowrap">
+        Graduating year
+      </div>
+
+      <div className="absolute top-[706px] left-[165px] h-[33px] flex items-center justify-center [font-family:'Inter',Helvetica] font-medium text-black text-3xl text-center tracking-[-0.60px] leading-[33.0px] whitespace-nowrap">
+        College
+      </div>
+
+      <div className="absolute top-[788px] left-[114px] h-11 flex items-center justify-center [font-family:'Inter',Helvetica] font-medium text-black text-3xl text-center tracking-[-0.15px] leading-[43.5px] whitespace-nowrap">
+        Transfer? (y/n)
+      </div>
+
+      <div className="absolute top-[884px] left-[45px] h-[33px] flex items-center justify-center [font-family:'Inter',Helvetica] font-medium text-black text-3xl text-center tracking-[-0.60px] leading-[33.0px] whitespace-nowrap">
+        Additional majors/minors
+      </div>
+
+      <div className="absolute top-0 left-0 w-[1285px] h-[314px] bg-[url(https://c.animaapp.com/rTAWbAbr/img/frame-1.png)] bg-cover bg-[50%_50%]">
+        <div className="absolute top-[72px] left-[353px] h-[94px] flex items-center justify-center [font-family:'Inter',Helvetica] font-black text-black text-[65px] text-center tracking-[-0.33px] leading-[94.3px] whitespace-nowrap">
+          UCSD ClassGraph
+        </div>
+      </div>
+
+      <div className="absolute top-[72px] left-[353px] h-[94px] flex items-center justify-center [font-family:'Inter',Helvetica] font-black text-black text-[65px] text-center tracking-[-0.33px] leading-[94.3px] whitespace-nowrap">
+        UCSD ClassGraph
+      </div>
+
+       <div className="w-[101px] top-[186px] left-[591px] inline-flex flex-wrap items-center gap-[16px_16px] absolute">
+        <div className="inline-flex items-center justify-center gap-2 px-4 py-3 relative flex-[0_0_auto] bg-black rounded-xl">
+          <div className="relative flex items-center justify-center w-fit mt-[-1.00px] [font-family:'Inter',Helvetica] font-medium text-white text-lg text-center tracking-[-0.09px] leading-[26.1px] whitespace-nowrap">
+            Let’s go
+          </div>
+        </div>
+      </div>
+
+      <p className="absolute top-[336px] left-[21px] w-[1259px] h-[108px] flex items-center justify-center [font-family:'Inter',Helvetica] font-medium text-black text-[25px] tracking-[-0.12px] leading-[36.3px]">
+        Many courses at UCSD have a variety of ways to take the necessary
+        prerequisites, making scheduling, course dependencies, and academic
+        planning difficult for students. ClassGraph helps our students
+        understand and plan their complex course requirements for their major
+        and graduation.
+      </p>
+    </div>
+  );
+};
