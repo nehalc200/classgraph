25957276ad3ed04cd0dc9f2e8c8191e8372519d6
Nehal Choudhary <nechoudhary@ucsd.edu>
2026-02-03 17:56:36 -0800

    Adjust layout and positions of UI elements

--- src/screens/Desktop/Desktop.jsx
+++ src/screens/Desktop/Desktop.jsx
@@ -4,7 +4,7 @@ export const Desktop = () => {
       className="overflow-hidden bg-[linear-gradient(180deg,rgba(255,255,255,1)_0%,rgba(255,255,255,0)_100%)] w-full min-w-[1280px] min-h-[1080px] relative"
       data-model-id="1:6"
     >
-      <div className="top-[698px] left-32 inline-flex flex-wrap items-center gap-[16px_16px] absolute">
+      <div className="top-[978px] left-32 inline-flex flex-wrap items-center gap-[16px_16px] absolute">
         <div className="inline-flex items-center justify-center gap-2 px-4 py-3 relative flex-[0_0_auto] bg-black rounded-xl">
           <div className="relative flex items-center justify-center w-fit mt-[-1.00px] [font-family:'Inter',Helvetica] font-medium text-white text-lg text-center tracking-[-0.09px] leading-[26.1px] whitespace-nowrap">
             Generate graph
@@ -12,19 +12,27 @@ export const Desktop = () => {
         </div>
       </div>
 
-      <div className="top-[465px] left-[509px] w-[70px] h-[547px] absolute bg-[#d9d9d9]" />
+      <div className="top-[505px] left-[509px] w-[700px] h-[547px] absolute bg-[#d9d9d9]" />
 
-      <div className="top-[calc(50.00%_-_35px)] left-[54px] w-[313px] h-[70px] absolute bg-[#d9d9d9]" />
+      {/* Vertical divider (leave as-is or adjust separately) */}
+      <div className="absolute top-[508px] left-[509px] w-[70px] h-[547px] bg-[#d9d9d9]" />
 
-      <div className="top-[calc(50.00%_-_35px)] left-[39px] w-[359px] h-[70px] absolute bg-[#d9d9d9]" />
+      {/* Major box */}
+      <div className="absolute top-[508px] left-[39px] w-[359px] h-[70px] bg-[#d9d9d9]" />
 
-      <div className="top-[calc(50.00%_-_55px)] left-[39px] w-[359px] h-[70px] absolute bg-[#d9d9d9]" />
+      {/* Graduating year box */}
+      <div className="absolute top-[598px] left-[39px] w-[359px] h-[70px] bg-[#d9d9d9]" />
 
-      <div className="top-[calc(50.00%_-_145px)] left-[39px] w-[359px] h-[70px] absolute bg-[#d9d9d9]" />
+      {/* College box */}
+      <div className="absolute top-[696px] left-[39px] w-[359px] h-[70px] bg-[#d9d9d9]" />
 
-      <div className="top-[calc(50.00%_-_235px)] left-[39px] w-[359px] h-[70px] absolute bg-[#d9d9d9]" />
+      {/* Transfer box */}
+      <div className="absolute top-[778px] left-[39px] w-[359px] h-[70px] bg-[#d9d9d9]" />
 
-      <div className="top-[calc(50.00%_-_325px)] left-[39px] w-[359px] h-[70px] absolute bg-[#d9d9d9]" />
+      {/* Additional majors/minors box */}
+      <div className="absolute top-[874px] left-[39px] w-[359px] h-[70px] bg-[#d9d9d9]" />
+
+
 
       <div className="absolute top-[518px] left-[177px] h-11 flex items-center justify-center [font-family:'Inter',Helvetica] font-medium text-black text-3xl text-center tracking-[-0.15px] leading-[43.5px] whitespace-nowrap">
         Major
